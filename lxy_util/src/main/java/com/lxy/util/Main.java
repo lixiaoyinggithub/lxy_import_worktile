2a96b6b481b4d35e3f83bd54e42b782ff50b7cc2
李孝赢 <444131509@qq.com>
2017-07-04 19:58:57 +0800

    去除无关信息

--- lxy_util/src/main/java/com/lxy/util/Main.java
+++ lxy_util/src/main/java/com/lxy/util/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		ExcelUtil excelUtil = new ExcelUtil("import.xls");
 
-		String pid = "4790ec0d0c424dfcb87dfa0086426e86";
+		String pid = "*****";
 		WorkTile workTile = new WorkTile(pid);
 		
 		
